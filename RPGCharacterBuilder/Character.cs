9950d932677a27ee272a5a804d0847861bdfc1f4
Andy <makimbell@gmail.com>
2019-05-27 12:46:20 -0400

    Added summaries to methods in Character class. Changed stat calculations

--- RPGCharacterBuilder/Character.cs
+++ RPGCharacterBuilder/Character.cs
@@ -11,9 +11,10 @@ namespace RPGCharacterBuilder
 
         // Level starts at 1. The other fields are determined by the character class
         private int _level;
-        private int _healthMultiplier, _strengthMultiplier, _defenseMultiplier;
+        private double _healthMultiplier, _strengthMultiplier, _defenseMultiplier;
 
-        public Character(string name, int healthMultiplier, int strengthMultiplier, int defenseMultiplier)
+        // Constructor with no level specified
+        public Character(string name, double healthMultiplier, double strengthMultiplier, double defenseMultiplier)
         {
             _name = name;
             _healthMultiplier = healthMultiplier;
@@ -22,16 +23,29 @@ namespace RPGCharacterBuilder
             _level = 1;
         }
 
+        // Constructor with level specified
+        public Character(string name, double healthMultiplier, double strengthMultiplier, double defenseMultiplier, int level)
+        {
+            _name = name;
+            _healthMultiplier = healthMultiplier;
+            _strengthMultiplier = strengthMultiplier;
+            _defenseMultiplier = defenseMultiplier;
+            _level = level;
+        }
+
         // Calculated properties: TotalHealth, Strength, Defense
-        // TODO: Incorporate item modifiers here as well. Also come up with a better formula
-        public int TotalHealth => _level * _healthMultiplier;
-        public int Strength => _level * _strengthMultiplier;
-        public int Defense => _level * _defenseMultiplier;
+        // TODO: Incorporate item modifiers here as well. Also come up with a better formula and possibly get rid of casting
+        public int TotalHealth => (int)(_level * _healthMultiplier);
+        public int Strength => (int)(_level * _strengthMultiplier);
+        public int Defense => (int)(_level * _defenseMultiplier);
 
         // Name getter (no setter -- cannot change name)
         public string Name { get => _name; }
 
-        // Prints character summary
+        /// <summary>
+        /// Prints character summary
+        /// </summary>
+        /// <param name="characterClass"></param>
         public void PrintCharacter(string characterClass)
         {
             Console.WriteLine("------------------------------");
@@ -42,23 +56,44 @@ namespace RPGCharacterBuilder
             Console.WriteLine("Defense: " + Defense);
         }
 
-        // Level up one level
+        /// <summary>
+        /// Levels up one level unless already at max level (99)
+        /// </summary>
         public void LevelUp()
         {
-            _level++;
+            if (_level != 99)
+            {
+                _level++;
+            }
         }
 
-        // Level up a specified number of levels
+        /// <summary>
+        /// Levels up a specified number of levels. If going over max level, new level will equal max level (99)
+        /// </summary>
+        /// <param name="levels"></param>
         public void LevelUp(int levels)
         {
-            _level += levels;
+            if (levels <= 0)
+            {
+                // Do nothing
+            }
+            else if (_level + levels > 99)
+            {
+                _level = 99;
+            }
+            else
+            {
+                _level = levels;
+            }
         }
 
-        //Equip a weapon, armor, or special item
-        // TODO: Complete
-        // TODO: Override methods so that you can pass in Weapon, Armor, or SpecialItem
+        /// <summary>
+        /// Equips a weapon, armor, or special item
+        /// </summary>
         public void Equip()
         {
+            // TODO: Complete
+            // TODO: Override methods so that you can pass in Weapon, Armor, or SpecialItem
             // TODO: Show what is currently equipped and ask to confirm
             Console.WriteLine("Equipping _____");
         }
--- RPGCharacterBuilder/Program.cs
+++ RPGCharacterBuilder/Program.cs
@@ -6,13 +6,14 @@ namespace RPGCharacterBuilder
     {
         static void Main(string[] args)
         {
-            Character characterOspov = new Character("Ospov", 1, 2, 3);
+            Character characterOspov = new Character("Ospov", 1.5, 2.2, 3.1);
             characterOspov.PrintCharacter("Barbarian");
 
             characterOspov.LevelUp(50);
             characterOspov.PrintCharacter("Barbarian");
 
             Console.ReadLine();
+
         }
     }
 }
