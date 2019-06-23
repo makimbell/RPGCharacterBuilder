4ca39a54dd8722e532d0ebc0759895c5b60fd604
Andy <makimbell@gmail.com>
2019-06-23 16:52:57 -0400

    Modified scope and structure of some variables and methods in Program class

--- RPGCharacterBuilder/Program.cs
+++ RPGCharacterBuilder/Program.cs
@@ -6,12 +6,36 @@ namespace RPGCharacterBuilder
 {
     class Program
     {
+        private static List<Character> characters = new List<Character>();
         static void Main()
         {
-            List<Character> characters = new List<Character>();
+            ReadCharactersFromFile();
+            ReadLineLoop();
+        }
+        private static void ReadCharactersFromFile()
+        {
+            // Read each line of the file into a string array. Each element of the array is one line of the file.
+            string[] lines = File.ReadAllLines("..\\..\\..\\CharacterData.txt");
+            // Read in the contents of CharacterData.txt. Each line represents an object (character or item)
+            foreach (string line in lines)
+            {
+                var currentData = line.Split(',');
 
-            ReadCharactersFromFile(characters);
-
+                // Character type, Name, Level
+                // Sample data: ["Barbarian","Andy",40]
+                switch (currentData[0])
+                {
+                    case "Barbarian":
+                        characters.Add(new Barbarian(currentData[1], Int32.Parse(currentData[2])));
+                        break;
+                    case "Ranger":
+                        characters.Add(new Ranger(currentData[1], Int32.Parse(currentData[2])));
+                        break;
+                }
+            }
+        }
+        private static void ReadLineLoop()
+        {
             var cmd = Console.ReadLine();
             while (cmd != "")
             {
@@ -32,30 +56,6 @@ namespace RPGCharacterBuilder
                     }
                 }
                 cmd = Console.ReadLine();
-            }
-
-        }
-
-        private static void ReadCharactersFromFile(List<Character> characters)
-        {
-            // Read each line of the file into a string array. Each element of the array is one line of the file.
-            string[] lines = File.ReadAllLines("..\\..\\..\\CharacterData.txt");
-            // Read in the contents of CharacterData.txt. Each line represents an object (character or item)
-            foreach (string line in lines)
-            {
-                var currentData = line.Split(',');
-
-                // Character type, Name, Level
-                // Sample data: ["Barbarian","Andy",40]
-                switch (currentData[0])
-                {
-                    case "Barbarian":
-                        characters.Add(new Barbarian(currentData[1], Int32.Parse(currentData[2])));
-                        break;
-                    case "Ranger":
-                        characters.Add(new Ranger(currentData[1], Int32.Parse(currentData[2])));
-                        break;
-                }
             }
         }
     }
